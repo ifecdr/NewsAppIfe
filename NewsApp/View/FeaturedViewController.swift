ce4ba7f45ec19675c039eb6dc1468e39d1c99d94
Ife <iadesida7@gmail.com>
2019-04-08 17:37:50 -0400

    Bugs

--- NewsApp/View/FeaturedViewController.swift
+++ NewsApp/View/FeaturedViewController.swift
@@ -19,7 +19,7 @@ class FeaturedViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         tableView.rowHeight = UITableView.automaticDimension
         viewModel.delegate = self
